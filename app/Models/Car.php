49a3df4054c5c5686e8422d8af32b3acf868f6ef
Dukai Tamás <dukai.tamas@student.jedlik.eu>
2024-05-07 16:51:26 +0200

    car rental app

---
+++ app/Models/Car.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Rental;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cars';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class, 'car_id','id');
+    }
+}
